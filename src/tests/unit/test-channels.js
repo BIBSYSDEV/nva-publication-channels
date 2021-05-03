b4c2f5e2c83808f6d7e3a27e04d6f7c25b419ad8
svenngjeto <svenn.gjeto@unit.no>
2021-05-03 09:03:09 +0200

    refactored tests

--- src/tests/unit/test-channels.js
+++ src/tests/unit/test-channels.js
@@ -3,37 +3,47 @@
 const channels = require('../../channels.js');
 const chai = require('chai');
 const expect = chai.expect;
-var event, context;
+let event, context;
 
-describe('Tests journal', function () {
-    it('verifies successful response', async () => {
-        const result = await channels.journal(event, context)
+function isAnObject(result) {
+    console.log("isAnObject")
+    expect(result).to.be.an('object');
+    expect(result.statusCode).to.equal(200);
+    expect(result.body).to.be.an('string');
+}
 
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
+function isAValidJournal(response) {
+    console.log("isAValidJournal")
+    expect(response).to.be.an('object');
+    expect(response.message).to.be.equal("hello journal");
+}
 
-        let response = JSON.parse(result.body);
+function isAValidPublisher(response) {
+    console.log("isAValidPublisher")
+    expect(response).to.be.an('object');
+    expect(response.message).to.be.equal("hello publisher");
+}
 
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello journal");
-        // expect(response.location).to.be.an("string");
+
+describe('Tests journal', async function () {
+    const result = await channels.journal(event, context)
+    it('verifies response is success and has a body', function () {
+        isAnObject(result)
+    });
+    const response = JSON.parse(result.body);
+    it('verifies response is a valid journal', async () => {
+        isAValidJournal(response);
     });
 });
 
-
-describe('Tests publisher', function () {
-    it('verifies successful response', async () => {
-        const result = await channels.publisher(event, context)
-
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello publisher");
-        // expect(response.location).to.be.an("string");
+describe('Tests publisher', async function () {
+    const result = await channels.publisher(event, context)
+    it('verifies response is success and has a body', function () {
+        isAnObject(result)
+    });
+    const response = JSON.parse(result.body);
+    it('verifies response is a valid publisher', async () => {
+        isAValidPublisher(response);
     });
 });
+
