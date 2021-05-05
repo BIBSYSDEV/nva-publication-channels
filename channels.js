ab5c317e9d9f2c5b7fa43faa3f887b32afdb67fc
svenngjeto <svenn.gjeto@unit.no>
2021-05-05 21:58:24 +0200

    reading parameters from body string

--- channels.js
+++ channels.js
@@ -2,9 +2,9 @@
 
 const { fetch } = require('./fetchData')
 
-function createRequestFromEvent (event) {
+function createRequestFromEvent (requestDetails) {
   return {
-    tabell_id: event.tableId,
+    tabell_id: requestDetails.tableId,
     api_versjon: 1,
     statuslinje: 'N',
     begrensning: 10,
@@ -17,7 +17,7 @@ function createRequestFromEvent (event) {
         variabel: 'Original Tittel',
         selection: {
           filter: 'like',
-          values: [event.searchTerm]
+          values: [requestDetails.searchTerm]
         }
       }
     ]
@@ -29,7 +29,7 @@ const searchHandler = async (event) => {
   try {
     console.log('REAL Channels')
     console.log(event)
-    const request = createRequestFromEvent(event)
+    const request = createRequestFromEvent(JSON.parse(event.body))
     const response = await fetch(request)
     data = response.data
     console.log('status=' + response.status)
--- fetchData.js
+++ fetchData.js
@@ -2,7 +2,8 @@ const axios = require('axios')
 const ChannelRegistryUrl = 'https://api.nsd.no/dbhapitjener/Tabeller/hentJSONTabellData'
 
 const fetch = async (request) => {
-  console.log('REAL fetcData, with request=' + JSON.stringify(request))
+  console.log('REAL fetcData, with request=')
+  console.log(request)
   const response = await axios.post(ChannelRegistryUrl, request)
   return response
 }
