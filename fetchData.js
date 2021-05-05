589a74cf4a950b30e83b4cec0485603c85b3d833
svenngjeto <svenn.gjeto@unit.no>
2021-05-05 20:46:18 +0200

    Reintroduced some commented code

--- fetchData.js
+++ fetchData.js
@@ -1,11 +1,10 @@
-// const axios = require('axios')
-// const ChannelRegistryUrl = 'https://api.nsd.no/dbhapitjener/Tabeller/hentJSONTabellData'
+const axios = require('axios')
+const ChannelRegistryUrl = 'https://api.nsd.no/dbhapitjener/Tabeller/hentJSONTabellData'
 
 const fetch = async (request) => {
   console.log('REAL fetcData, with request=' + JSON.stringify(request))
-  return { status: 500 }
-  // const response = await axios.post(ChannelRegistryUrl, request)
-  // return response
+  const response = await axios.post(ChannelRegistryUrl, request)
+  return response
 }
 
 exports.fetch = fetch
