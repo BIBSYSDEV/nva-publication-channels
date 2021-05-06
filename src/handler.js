9a17853747a2520ac3a00cd576224c16ba3eca17
svenngjeto <svenn.gjeto@unit.no>
2021-05-06 14:50:16 +0200

    Added linting and testing setup

--- src/handler.js
+++ src/handler.js
@@ -1,46 +1,41 @@
-const AWS = require('aws-sdk')
-// Create client outside of handler to reuse
-const lambda = new AWS.Lambda()
-
-// Handler
 exports.handler = async function (event, context) {
-    try {
-        let queryParameters = await getQueryParameters(event)
-        return formatResponse(serialize(queryParameters))
-    } catch (error) {
-        return formatError(error)
-    }
+  try {
+    const queryParameters = await getQueryParameters(event)
+    return formatResponse(serialize(queryParameters))
+  } catch (error) {
+    return formatError(error)
+  }
 }
 
-var formatResponse = function (body) {
-    var response = {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "isBase64Encoded": false,
-        "body": body
-    }
-    return response
+const formatResponse = function (body) {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    isBase64Encoded: false,
+    body: body
+  }
+  return response
 }
 
-var formatError = function (error) {
-    var response = {
-        "statusCode": error.statusCode,
-        "headers": {
-            "Content-Type": "text/plain",
-            "x-amzn-ErrorType": error.code
-        },
-        "isBase64Encoded": false,
-        "body": error.code + ": " + error.message
-    }
-    return response
+const formatError = function (error) {
+  const response = {
+    statusCode: error.statusCode,
+    headers: {
+      'Content-Type': 'text/plain',
+      'x-amzn-ErrorType': error.code
+    },
+    isBase64Encoded: false,
+    body: error.code + ': ' + error.message
+  }
+  return response
 }
 // Use SDK client
-var getQueryParameters = function (event) {
-    return {query: event.query}
+const getQueryParameters = function (event) {
+  return { query: event.query }
 }
 
-var serialize = function (object) {
-    return JSON.stringify(object, null, 2)
+const serialize = function (object) {
+  return JSON.stringify(object, null, 2)
 }
