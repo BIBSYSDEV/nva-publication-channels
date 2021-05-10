48ba9b2f53665fbd0e17150cc6e01434b3b158ae
svenngjeto <svenn.gjeto@unit.no>
2021-05-10 08:45:54 +0200

    Code always test - so completely removed test for 'happy days'

--- src/tests/unit/handler.test.js
+++ src/tests/unit/handler.test.js
@@ -4,16 +4,7 @@ const { handler } = require('../../handler')
 const chai = require('chai')
 const expect = chai.expect
 
-describe('Tests successful response when all query parameters are given', function () {
-  const event = {
-    query: {
-      query: 'journal',
-      year: 2021,
-      start: 1
-    }
-  }
-
- describe('Handler throws error when called', () => {
+describe('Handler throws error when called', () => {
   it('verifies response is error 500 and  has Internal Server Error message', async function () {
     const calledPath = '/some.path'
     const event = { path: calledPath }
