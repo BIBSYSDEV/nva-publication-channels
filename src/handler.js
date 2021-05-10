3ac63504a9e52378f848c6d23df286444174dada
svenngjeto <svenn.gjeto@unit.no>
2021-05-10 13:26:11 +0200

    Codefixup after QA

--- src/handler.js
+++ src/handler.js
@@ -1,50 +1,32 @@
-class ProblemResponse extends Error {
-  constructor (code, detail, requestUri) {
-    super(detail)
-    this.statusCode = code
-    this.detail = detail
-    this.instance = requestUri
-  }
+const ProblemDocument = require('http-problem-details').ProblemDocument
+
+exports.handler = async function (event, context) {
+  return generateProblemResponse(event)
 }
 
 exports.handler = async function (event, context) {
-  let response
-  try {
-    throwGeneralError(event)
-    response = formatResponse('This message should not be visible..')
-  } catch (error) {
-    console.log(error)
-    response = generateProblemResponse(error)
-  }
-  return response
+  return generateProblemResponse(event)
 }
 
-const formatResponse = function (body) {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    isBase64Encoded: false,
-    body: JSON.stringify(body)
-  }
-  return response
-}
-
-const generateProblemResponse = function (error) {
-  const response = {
-    statusCode: error.statusCode,
+const generateProblemResponse = function (event) {
+  const INTERNAL_SERVER_ERROR = 500
+  const detail = 'Your request cannot be processed at this time because of an internal server error'
+  const path = event.path
+  console.log(detail, path)
+  return {
+    statusCode: INTERNAL_SERVER_ERROR,
     headers: {
       'Content-Type': 'application/problem+json',
-      'x-amzn-ErrorType': error.code
+      'x-amzn-ErrorType': INTERNAL_SERVER_ERROR
     },
     isBase64Encoded: false,
-    body: JSON.stringify(error)
+    body: JSON.stringify(
+      new ProblemDocument(
+        {
+          status: INTERNAL_SERVER_ERROR,
+          detail: detail,
+          instance: event.path
+        }
+      ))
   }
-  return response
-}
-
-const throwGeneralError = function (event) {
-  console.log(event)
-  throw new ProblemResponse(500, 'Your request cannot be processed at this time because of an internal server error', event.path)
 }
