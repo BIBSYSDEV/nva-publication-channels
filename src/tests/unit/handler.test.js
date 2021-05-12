556fb2c89e0fdc6262ef7b1c345e6577f440d356
svenngjeto <svenn.gjeto@unit.no>
2021-05-12 14:19:44 +0200

    Arrowified tests

--- src/tests/unit/handler.test.js
+++ src/tests/unit/handler.test.js
@@ -21,43 +21,32 @@ describe('Handler throws error when called with illegal path', () => {
 })
 
 describe('Handler throws error when called with illegal method and correct path', () => {
-  it('verifies response is error 40%-NOT FOUND, and has Not Found error message', async function () {
-    const calledPath = '/journal'
-    const httpMethod = 'PUT'
-    const event = { path: calledPath, httpMethod: httpMethod }
-    const response = await handler(event)
-    expect(response.statusCode).to.equal(httpStatus.METHOD_NOT_ALLOWED)
-    const responseBody = JSON.parse(response.body)
-    expect(responseBody.instance).to.equal(calledPath)
-    expect(responseBody.status).to.equal(httpStatus.METHOD_NOT_ALLOWED)
-    expect(responseBody.detail).to.equal("Your request cannot be processed at this time due of 'Method Not Allowed'")
-    expect(responseBody.title).to.equal('Method Not Allowed')
-    expect(responseBody.type).to.equal('about:blank')
-  })
+  ['/journal', '/publisher'].map(calledPath => (
+    it(`Put on path ${calledPath} returns 405-Method Not Allowed, and has Method Not Allowed error message`, async function () {
+      const httpMethod = 'PUT'
+      const event = { path: calledPath, httpMethod: httpMethod }
+      const response = await handler(event)
+      expect(response.statusCode).to.equal(httpStatus.METHOD_NOT_ALLOWED)
+      const responseBody = JSON.parse(response.body)
+      expect(responseBody.instance).to.equal(calledPath)
+      expect(responseBody.status).to.equal(httpStatus.METHOD_NOT_ALLOWED)
+      expect(responseBody.detail).to.equal("Your request cannot be processed at this time due of 'Method Not Allowed'")
+      expect(responseBody.title).to.equal('Method Not Allowed')
+      expect(responseBody.type).to.equal('about:blank')
+    })
+  ))
 })
 
 describe("Handler verifies route /journal; path '/journal', httpMethod.GET and pathParameter 'pid'", () => {
-  it('verifies response is 200 and  has empty body', async function () {
-    const calledPath = '/journal'
-    const journalPid = 'just.a.pid'
-    const httpMethod = 'GET'
-    const emptyBody = '{}'
-    const event = { path: calledPath, pathParameters: journalPid, httpMethod: httpMethod }
-    const response = await handler(event)
-    expect(response.statusCode).to.equal(200)
-    expect(response.body).to.equal(emptyBody)
-  })
-})
-
-describe("Handler verifies route /publisher; path '/publisher', httpMethod.GET and pathParameter 'pid'", () => {
-  it('verifies response is 200 and  has empty body', async function () {
-    const calledPath = '/publisher'
-    const journalPid = 'just.a.pid'
-    const httpMethod = 'GET'
-    const emptyBody = '{}'
-    const event = { path: calledPath, pathParameters: journalPid, httpMethod: httpMethod }
-    const response = await handler(event)
-    expect(response.statusCode).to.equal(200)
-    expect(response.body).to.equal(emptyBody)
-  })
+  ['/journal', '/publisher'].map(calledPath => (
+    it(`GET ${calledPath} returns 200 OK and has empty body`, async function () {
+      const journalPid = 'just.a.pid'
+      const httpMethod = 'GET'
+      const emptyBody = '{}'
+      const event = { path: calledPath, pathParameters: journalPid, httpMethod: httpMethod }
+      const response = await handler(event)
+      expect(response.statusCode).to.equal(200)
+      expect(response.body).to.equal(emptyBody)
+    })
+  ))
 })
