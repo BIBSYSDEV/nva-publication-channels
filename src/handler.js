8e82ee964ca58908bbb93cf16b8103daa5268b0f
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-05-18 08:58:25 +0200

    Restructure code

--- src/handler.js
+++ src/handler.js
@@ -3,14 +3,15 @@ const httpStatus = require('http-status-codes')
 const logger = require('pino')({ useLevelLabels: true })
 
 logger.info('Logger initialized')
+
+const routes = []
+
 exports.handler = async (event, context) => {
   const response = 'path' in event && !routes.includes(event.path)
     ? { code: httpStatus.NOT_FOUND, message: `The requested resource ${event.path} could not be found` }
     : { code: httpStatus.INTERNAL_SERVER_ERROR, message: 'Your request cannot be processed at this time due to an internal server error' }
   return errorResponse(response, event)
 }
-
-const routes = []
 
 const errorResponse = (response, event) => {
   const path = 'path' in event ? event.path : 'Undefined path'
