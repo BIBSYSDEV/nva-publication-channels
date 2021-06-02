1726b9d238df42d91a09f44d099d3cb1b2944783
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-06-03 00:37:07 +0200

    Belt and braces check for objectiness of input parameters

--- src/errors/MethodNotAllowedError.js
+++ src/errors/MethodNotAllowedError.js
@@ -1,6 +1,6 @@
 class MethodNotAllowedError extends Error {
   constructor (event) {
-    const pathParameters = event.pathParameters instanceof Object
+    const pathParameters = event.pathParameters instanceof Object && !Array.isArray(event.pathParameters)
       ? Object.values(event.pathParameters).map(param => '/' + param).join('')
       : ''
     super(`Method '${event.httpMethod}' not allowed for resource '${event.path + pathParameters}'`)
