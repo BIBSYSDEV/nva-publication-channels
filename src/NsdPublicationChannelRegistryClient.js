f0e6b38ddc40f1f556cf057c4af518411008faaf
svenngjeto <svenn.gjeto@unit.no>
2021-06-07 22:34:52 +0200

    created method for extracting hits from external NSD response

--- src/NsdPublicationChannelRegistryClient.js
+++ src/NsdPublicationChannelRegistryClient.js
@@ -4,14 +4,19 @@ const axios = require('axios')
 const RemoteJournalResponse = require('./response/RemoteJournalResponse')
 const RemotePublisherResponse = require('./response/RemotePublisherResponse')
 
-const responseWithBody = (body, type, year) => {
+const isJournal = (type) => type === 'journal'
+
+const extractHits = (year, type, body) => {
   const response = []
   const level = `Nivå ${year}`
-  if (type === 'journal') {
-    body.forEach(item => response.push(new RemoteJournalResponse(item['Tidsskrift id'], year, item['Original tittel'], item.Url, item[level], item.Aktiv, item['Online ISSN'], item['Print ISSN'], item['NPI Fagfelt'], item['Open Access'], item['Språk'], item.Utgiver)))
-  } else {
-    body.forEach(item => response.push(new RemotePublisherResponse(item['Forlag id'], year, item['Original tittel'], item.Url, item[level], item.Aktiv)))
-  }
+  isJournal(type)
+    ? body.forEach(item => response.push(new RemoteJournalResponse(item['Tidsskrift id'], year, item['Original tittel'], item.Url, item[level], item.Aktiv, item['Online ISSN'], item['Print ISSN'], item['NPI Fagfelt'], item['Open Access'], item['Språk'], item.Utgiver)))
+    : body.forEach(item => response.push(new RemotePublisherResponse(item['Forlag id'], year, item['Original tittel'], item.Url, item[level], item.Aktiv)))
+  return response
+}
+
+const responseWithBody = (body, type, year) => {
+  const response = extractHits(year, type, body)
   return {
     statusCode: 200,
     headers: {
